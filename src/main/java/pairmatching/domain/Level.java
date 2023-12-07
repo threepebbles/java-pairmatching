e862116d52cb27ac6fcb85ae3d97b33d994cf2a4
threepebbles <threepebbles@naver.com>
2023-12-07 17:08:23 +0900

    Feat(Level): 레벨 이름으로 레벨 객체를 반환하는 기능 추가

--- src/main/java/pairmatching/domain/Level.java
+++ src/main/java/pairmatching/domain/Level.java
@@ -1,6 +1,8 @@
 package pairmatching.domain;
 
 import java.util.Arrays;
+import java.util.List;
+import pairmatching.error.ErrorMessage;
 
 public enum Level {
     LEVEL1("레벨1"),
@@ -16,6 +18,16 @@ public enum Level {
     }
 
     // 추가 기능 구현
+    public static Level getLevelByName(String name) {
+        List<Level> candidates = Arrays.stream(values())
+                .filter(level -> level.name.equals(name))
+                .toList();
+        if (candidates.size() != 1) {
+            throw new IllegalArgumentException(ErrorMessage.getErrorMessage("올바르지 않은 코스 이름입니다."));
+        }
+        return candidates.get(0);
+    }
+
     public static boolean exist(String name) {
         return Arrays.stream(values())
                 .filter(level -> level.name.equals(name))
