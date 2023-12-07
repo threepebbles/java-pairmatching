b347a9aeec531b4bbf7e632e1e087a175341a51f
threepebbles <threepebbles@naver.com>
2023-12-07 14:58:48 +0900

    Feat(Parser): delimiter 기준으로 파싱하는 기능 추가

---
+++ src/main/java/util/Parser.java
@@ -0,0 +1,11 @@
+package util;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Parser {
+    public static List<String> parseWithDelimiter(String userInput, String delimiter) {
+        return Arrays.stream(userInput.split(delimiter, -1))
+                .toList();
+    }
+}
