c11dd1481edf6c0d133ece3c471f96c60f3581e9
threepebbles <threepebbles@naver.com>
2023-12-07 18:13:49 +0900

    Feat: [ERROR]로 시작하는 에러 메세지를 출력하도록 수정

--- src/main/java/pairmatching/view/MainView.java
+++ src/main/java/pairmatching/view/MainView.java
@@ -3,6 +3,7 @@ package pairmatching.view;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 import pairmatching.error.ErrorHandler;
+import pairmatching.error.ErrorMessage;
 
 public class MainView {
     private final static List<String> OPTIONS = List.of("1", "2", "3", "Q");
@@ -26,7 +27,7 @@ public class MainView {
 
     private static void checkOption(String userInput) {
         if (!OPTIONS.contains(userInput)) {
-            throw new IllegalArgumentException("유효하지 않은 옵션입니다.");
+            throw new IllegalArgumentException(ErrorMessage.getErrorMessage("유효하지 않은 옵션입니다."));
         }
     }
 }
