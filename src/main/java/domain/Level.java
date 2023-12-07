6469ed64f4abec1beefb4be9d828fc031c518cf5
threepebbles <threepebbles@naver.com>
2023-12-07 15:15:26 +0900

    Feat(Level): 존재하는 레벨인지 확인하는 기능 추가

--- src/main/java/domain/Level.java
+++ src/main/java/domain/Level.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Arrays;
+
 public enum Level {
     LEVEL1("레벨1"),
     LEVEL2("레벨2"),
@@ -14,4 +16,9 @@ public enum Level {
     }
 
     // 추가 기능 구현
+    public static boolean exist(String name) {
+        return Arrays.stream(values())
+                .filter(level -> level.name.equals(name))
+                .count() == 1;
+    }
 }
