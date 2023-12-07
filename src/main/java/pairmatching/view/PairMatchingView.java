7164db0728f5b56dd9acee432145289bcedb9e41
threepebbles <threepebbles@naver.com>
2023-12-07 17:44:31 +0900

    Refactor: 함수 네이밍 변경

--- src/main/java/pairmatching/view/PairMatchingView.java
+++ src/main/java/pairmatching/view/PairMatchingView.java
@@ -2,9 +2,7 @@ package pairmatching.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
-import pairmatching.domain.Course;
-import pairmatching.domain.Level;
-import pairmatching.domain.Mission;
+import pairmatching.domain.PairMatchingResult;
 import pairmatching.error.ErrorHandler;
 import pairmatching.error.ErrorMessage;
 import pairmatching.error.Validator;
@@ -29,12 +27,18 @@ public class PairMatchingView {
     public static void printDescription() {
         System.out.println(PAIR_MATCHING_DESCRIPTION);
     }
+
     public static void printRematchMessage() {
         System.out.println("매칭 정보가 있습니다. 다시 매칭하시겠습니까?");
         System.out.println("네 | 아니오");
     }
-    public static void printInitializePairMatchingResults() {
+
+    public static void printClearPairMatchingResults() {
         System.out.println("초기화 되었습니다.");
+    }
+
+    public static void printPairMatchingResult(PairMatchingResult pairMatchingResult) {
+
     }
 
     public static MissionDto scanMission() {
@@ -56,8 +60,8 @@ public class PairMatchingView {
     }
 
     private static void checkRematch(String userInput) {
-        if(!(userInput.equals("네") || userInput.equals("아니오"))) {
-            throw new IllegalArgumentException(ErrorMessage.getErrorMessage("네, 아니오로 입력해야합니다."))
+        if (!(userInput.equals("네") || userInput.equals("아니오"))) {
+            throw new IllegalArgumentException(ErrorMessage.getErrorMessage("네, 아니오로 입력해야합니다."));
         }
     }
 
