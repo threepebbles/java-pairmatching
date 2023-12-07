94ce4222a216c6d8a73330c28a1f930249b02bad
threepebbles <threepebbles@naver.com>
2023-12-07 14:28:04 +0900

    Feat(ErrorHandler): 에러 발생 시 에러 메세지 출력 후 재시도 하도록 하는 기능 추가

---
+++ src/main/java/error/ErrorHandler.java
@@ -0,0 +1,28 @@
+package error;
+
+import java.util.function.Supplier;
+import view.ErrorView;
+
+public class ErrorHandler {
+    public static <T> Object retryUntilSuccessWithReturn(Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException e) {
+                ErrorView.println(e.getMessage());
+            }
+        }
+    }
+
+    public static void retryUntilSuccess(Runnable toRun) {
+        while (true) {
+            try {
+                toRun.run();
+                return;
+            } catch (IllegalArgumentException e) {
+                ErrorView.println(e.getMessage());
+            }
+        }
+    }
+}
+// Feat(ErrorHandler): 에러 발생 시 에러 메세지 출력 후 재시도 하도록 하는 기능 추가
