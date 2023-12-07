4fb414ab3b9f2c685d1cbe871e410cd787209fa5
threepebbles <threepebbles@naver.com>
2023-12-07 17:06:12 +0900

    Feat(Mission): 레벨, 미션이름으로 해당되는 미션 객체 반환하는 기능 추가

--- src/main/java/pairmatching/domain/Mission.java
+++ src/main/java/pairmatching/domain/Mission.java
@@ -1,6 +1,8 @@
 package pairmatching.domain;
 
 import java.util.Arrays;
+import java.util.List;
+import pairmatching.error.ErrorMessage;
 
 public enum Mission {
     CAR_RACING(Level.LEVEL1, "자동차경주"),
@@ -18,6 +20,16 @@ public enum Mission {
     Mission(Level level, String name) {
         this.level = level;
         this.name = name;
+    }
+
+    public static Mission valueOf(Level level, String name) {
+        List<Mission> candidates = Arrays.stream(values())
+                .filter(mission -> mission.level.equals(level) && mission.name.equals(name))
+                .toList();
+        if(candidates.size() != 1) {
+            throw new IllegalArgumentException(ErrorMessage.getErrorMessage("존재하지 않는 미션입니다."))
+        }
+        return candidates.get(0);
     }
 
     public static boolean exist(Level level, String name) {
